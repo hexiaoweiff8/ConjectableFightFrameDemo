7346371fd64fc3f5799105d1b3b446eff806f3d8
USER-20161022KS\Administrator <hexiaoweiff8@sina.com>
2018-10-13 17:55:00 +0800

    注释逻辑提交

--- Assets/script/AI/Member/Member.cs
+++ Assets/script/AI/Member/Member.cs
@@ -19,6 +19,16 @@ namespace Assets.script.AI.Member
             // 随机获取目标位置
             // 前进
             var path = AStarPathFinding.SearchRoad(null, 1, 1, 1, 1, 1, 1);
+
+            // 如果到达路径尽头, 重新寻路继续前进
+            // 重点是结构
+
+            // 性能-拆分结构
+
+            // 单帧任务数
+
+            // 超出任务延帧执行
+
         }
         // 移动-格子
 
